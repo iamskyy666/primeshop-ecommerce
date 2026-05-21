16d653bb76ab78043c5db7393695f438ec079965
Soumadip <39thdecember@gmail.com>
2026-05-21 23:37:45 +0530

    add react-router-dom (v6)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
+import { Outlet } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
@@ -9,10 +9,10 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-         <HomeScreen/>
+          <Outlet />
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
