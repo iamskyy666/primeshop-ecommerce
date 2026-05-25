052d3fccc1704c287b1a4517e7096a0e2ef26b5e
Soumadip <39thdecember@gmail.com>
2026-05-25 13:49:37 +0530

    Add Products API Slice + Get Products Endpoint 🟣

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,46 @@
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    async function fetchProducts() {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    }
-    fetchProducts();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchProducts() {
+  //     const { data } = await axios.get("/api/products");
+  //     setProducts(data);
+  //   }
+  //   fetchProducts();
+  // }, []);
+
+  //! 💡 Using RTK-Query instead (better,simpler)
+
+  const { data: products, isLoading, isError, error } = useGetProductsQuery();
 
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => {
-          return (
-            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-              <Product product={product} />
-            </Col>
-          );
-        })}
-      </Row>
+      {isLoading ? (
+        <h2>Loading... ⌛</h2>
+      ) : isError ? (
+        <h2>
+          ⚠️ {error?.data?.message || error?.error} {console.log(error)}
+        </h2>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products?.map((product) => {
+              return (
+                <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                  <Product product={product} />
+                </Col>
+              );
+            })}
+          </Row>
+        </>
+      )}
     </>
   );
 }
---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,16 @@
+import { PRODUCTS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: PRODUCTS_URL, // No fetch/axios requests needed.
+      }),
+      keepUnusedDataFor: 5,
+    }),
+  }),
+});
+
+// convention - automatic naming
+export const { useGetProductsQuery } = productsApiSlice;
