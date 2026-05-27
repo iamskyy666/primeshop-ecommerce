51b42467d758890f67e2bdff862e7cf671a69e9e
Soumadip <39thdecember@gmail.com>
2026-05-27 21:57:35 +0530

    feat: user-profile endpoints 🔵

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -36,14 +36,10 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 };
 
 // password-hashing
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    next();
-  }
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 const User = mongoose.model("User", userSchema);
